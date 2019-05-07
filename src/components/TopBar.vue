f723b143a7a9c0c48c6da41002703b0e3dfd78ca
Govinda Khatiwada <naturalfiber@gmail.com>
2019-05-07 11:33:21 +0545

    Update TopBar.vue

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -6,18 +6,12 @@
         :to="{
           name: 'home',
         }">
-        WP Vue
+        eLatestNews.com
       </router-link>
     </h1>
 
     <div>
-      <p>
-        Currently presenting content from <a :href="sourceURL">{{ splitEndpoint[2] }}</a>.
-      </p>
-
-      <button @click="showUpdater">
-        Change Endpoint
-      </button>
+     
     </div>
   </nav>
 </template>
